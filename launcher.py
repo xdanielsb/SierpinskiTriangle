b0c118910d76e5639a17e703b5fa0ed802bc1be0
Daniel Santos <dfsantosbu@unal.edu.co>
2017-11-02 09:52:05 -0500

    Add readme and results.

--- launcher.py
+++ launcher.py
@@ -6,6 +6,5 @@ from logic import init
 if(__name__ == "__main__"):
     l = 10000
     t1 = t(p(0,0), p(l/2, l*m.sin(60*m.pi/180)), p(l,0))
-    t1.show()
     tree = init(t1)
     plot(tree, l)
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Sierpinski   triangle
+
+## Image with 87381 Triangles
+![Triangle](results/figure_87381_triangles.png)
+
+>"Author : Daniel S"
